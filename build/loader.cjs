965d7b58b7e4b2479e9b166c5944cae249e0bcea
Shurygina Julia <melam.2916@gmail.com>
2024-12-09 13:51:55 +0300

    feat:homework-1

--- build/loader.cjs
+++ build/loader.cjs
@@ -1,2 +1,10 @@
-const yaml = require('yaml');
-const fs = require('node:fs');
+const yaml = require("yaml");
+const fs = require("node:fs");
+
+// Добавляем обработчик для расширения .yml
+require.extensions[".yml"] = function (module, filename) {
+  // Читаем содержимое YAML-файла
+  const content = fs.readFileSync(filename, "utf8");
+  // Парсим YAML и экспортируем как JSON
+  module.exports = yaml.parse(content);
+};
--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,50 @@
-import yaml from 'yaml';
-import path from 'node:path';
-import fs from 'node:fs/promises';
+import yaml from "yaml";
+import path from "node:path";
+import fs from "node:fs/promises";
+
+// Function to load YAML
+export async function loadYAML(modulePath) {
+  console.log("loadYAML");
+  const currentModulePath = new URL(import.meta.url).pathname;
+  const currentModuleDir = path.dirname(currentModulePath);
+  const filePath = path.resolve(path.join(currentModuleDir, modulePath));
+
+  try {
+    const fileContent = await fs.readFile(filePath, "utf8");
+    return yaml.parse(fileContent);
+  } catch (error) {
+    console.error(`Error loading YAML file: ${filePath}`, error);
+    throw error;
+  }
+}
+
+export function createLoader() {
+  return {
+    get: async (specifier, resolveContext) => {
+      const url = new URL(specifier, resolveContext.parentURL);
+      if (url.pathname.endsWith(".yml") || url.pathname.endsWith(".yaml")) {
+        try {
+          const yamlData = await loadYAML(url.pathname.slice(1));
+          return {
+            format: "module",
+            module: {
+              default: yamlData,
+            },
+          };
+        } catch (error) {
+          console.error(`Error loading module: ${specifier}`, error);
+          throw error;
+        }
+      }
+      return null;
+    },
+  };
+}
+
+// Register loader
+const loader = createLoader();
+if (typeof globalThis.__MODULE_LOADERS__ !== "undefined") {
+  globalThis.__MODULE_LOADERS__.push(loader);
+} else {
+  globalThis.__MODULE_LOADERS__ = [loader];
+}
